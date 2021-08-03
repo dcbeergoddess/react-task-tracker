e36697e6c681c5cd2f8e1db81d5161c30db4d25c
Rachel Murray <dcbeergoddess@gmail.com>
2021-08-03 16:11:30 -0400

    adding tasks with global state and useState hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'DMV Alumni Meeting',
+      day: 'July 30th at 1pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Log Coding Hours',
+      day: 'August 4th at 4pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Send Finished Crochet Sweater',
+      day: 'August 6th at 11am',
+      reminder: false,
+    }
+  
+])
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
