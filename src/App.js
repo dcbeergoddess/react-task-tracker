c39449b9370fcfe66a720ec8037a6262c4900e7b
Rachel Murray <dcbeergoddess@gmail.com>
2021-08-04 19:44:12 -0400

    useLocation hook to hide button when not on index page

--- src/App.js
+++ src/App.js
@@ -1,99 +1,107 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Tasks from './components/Tasks';
-import AddTask from './components/AddTask'
+import AddTask from './components/AddTask';
+import About from './components/About';
 
 function App() {
-  const [showAddTask, setShowAddTask] = useState(false)
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
     const getTasks = async () => {
-      const tasksFromServer = await fetchTasks()
-      setTasks(tasksFromServer)
-    }
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
 
-    getTasks()
-  }, []) //DEPENDENCY ARRAY
+    getTasks();
+  }, []); //DEPENDENCY ARRAY
 
   //FETCH TASKS
   const fetchTasks = async () => {
-    const res = await fetch('http://localhost:5000/tasks')
-    const data = await res.json()
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
 
-    return data
-  }
+    return data;
+  };
 
   //FETCH TASK
-  const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`)
-    const data = await res.json()
+  const fetchTask = async id => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
 
-    return data
-  }
+    return data;
+  };
 
   //ADD TASK
-  const addTask = async (task) => {
+  const addTask = async task => {
     const res = await fetch('http://localhost:5000/tasks', {
       method: 'POST',
       headers: {
-        'Content-type' : 'application/json'
+        'Content-type': 'application/json',
       },
-      body: JSON.stringify(task)
-    })
+      body: JSON.stringify(task),
+    });
 
-    const data = await res.json()
+    const data = await res.json();
 
-    setTasks([...tasks, data])
+    setTasks([...tasks, data]);
 
     //CREATED ID WITHOUT BACKEND JSON SERVER IN PLACE
     // const id = Math.floor(Math.random() * 10000) + 1
     // const newTask = {id, ...task}
     // setTasks([...tasks, newTask])
-  }
+  };
 
   //DELETE TASK
-  const deleteTask = async (id) => {
+  const deleteTask = async id => {
     await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'DELETE'
-    })
+      method: 'DELETE',
+    });
     //for each task you want to filter where the task id is not equal to the id
     setTasks(tasks.filter(task => task.id !== id));
   };
 
   //TOGGLE REMINDER
-  const toggleReminder = async (id) => {
-    const taskToToggle = await fetchTask(id)
-    const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+  const toggleReminder = async id => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
 
     const res = await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'PATCH',
       headers: {
-        'Content-type': 'application/json'
+        'Content-type': 'application/json',
       },
-      body: JSON.stringify(updTask)
-    })
+      body: JSON.stringify(updTask),
+    });
 
-    const data = await res.json()
+    const data = await res.json();
 
-    //use map to toggle --> map through `tasks` in our state and for each `task` 
-    //where `task.id` in current iteration is equal to the id that's passed in 
-    //then we have specific object : (else the task) 
+    //use map to toggle --> map through `tasks` in our state and for each `task`
+    //where `task.id` in current iteration is equal to the id that's passed in
+    //then we have specific object : (else the task)
     //we want to copy && spread across all the task properties and values in object (of the task that matches) but want to change the reminder so the reminder i'm going to set is opposite of whatever that specific task reminder is
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: data.reminder } : task))
-  }
+    setTasks(tasks.map(task => (task.id === id ? { ...task, reminder: data.reminder } : task)));
+  };
 
   return (
-    <div className="container">
-      <Header 
-        onAdd={() => setShowAddTask(!showAddTask)}
-        showAdd={showAddTask} 
-      />
-      {/* If showAddTask is true we show AddTask else we do nothing */}
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {/* IF THERE ARE TASKS, show Tasks, else show message */}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        <Route path='/' exact render={(props) => (
+          <>
+            {/* If showAddTask is true we show AddTask else we do nothing */}
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {/* IF THERE ARE TASKS, show Tasks, else show message */}
+            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
+          </>
+        )} />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
